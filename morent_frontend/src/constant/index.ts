fd19f591af058a21a16b940d68397934a11b988d
wirayuda <wirayuda233@gmail.com>
2023-08-10 15:47:10 +0800

    project setup for frontend and create from in checkout page

---
+++ morent_frontend/src/constant/index.ts
@@ -0,0 +1,20 @@
+import { amex, discover, mastercard, visa } from '../assets/icons';
+
+export const icons = [
+  {
+    label: 'visa icon',
+    icon: visa,
+  },
+  {
+    label: 'mastercard icon',
+    icon: mastercard,
+  },
+  {
+    label: 'american express icon',
+    icon: amex,
+  },
+  {
+    label: 'discover icon',
+    icon: discover,
+  },
+];
