3b157aecbaf06ccd8e0da1f82344efcd04e7763b
Hannah-Liao <hanna.liao@outlook.com>
2023-08-10 19:55:56 +0100

    fix footer responsive

--- morent_frontend/src/components/Logo.tsx
+++ morent_frontend/src/components/Logo.tsx
@@ -1,6 +1,6 @@
 const Logo = () => {
   return (
-    <h1 className='text-[#3563E9] text-[2rem] font-[700] leading-[120%] tracking-[0.0625rem]'>
+    <h1 className='text-[#3563E9] text-[1.5rem] sm:text-[2rem] font-[700] leading-[120%] tracking-[0.0625rem]'>
       MORENT
     </h1>
   );
