89864cc66cf12d63ea4bd8f82f1b4c792cd65815
Hannah-Liao <hanna.liao@outlook.com>
2023-08-11 17:03:33 +0100

    feat: add mode drop down

---
+++ morent_frontend/src/components/ModeDropdown.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { SunIcon, MoonIcon, DesktopIcon } from '@radix-ui/react-icons';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../components/ui/dropdown-menu';
+
+export function DropdownMenuDemo() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        {isDarkMode ? (
+          <MoonIcon className='fill-[#3D5278] w-[1.6rem] h-[1.6rem]' />
+        ) : (
+          <SunIcon className='fill-[#3D5278] w-[1.6rem] h-[1.6rem]' />
+        )}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-50'>
+        <DropdownMenuGroup>
+          <DropdownMenuItem onSelect={() => setIsDarkMode((prev) => !prev)}>
+            <SunIcon className='mr-2 h-4 w-4' />
+            <span>Light</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem onSelect={() => setIsDarkMode((prev) => !prev)}>
+            <MoonIcon className='mr-2 h-4 w-4' />
+            <span>Dark</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onSelect={() => {
+              if (
+                window.matchMedia &&
+                window.matchMedia('(prefers-color-scheme: dark)').matches
+              ) {
+                setIsDarkMode(true);
+              } else {
+                setIsDarkMode(false);
+              }
+            }}
+          >
+            <DesktopIcon className='mr-2 h-4 w-4' />
+            <span>System</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- morent_frontend/src/components/Navbar/NavBar.tsx
+++ morent_frontend/src/components/Navbar/NavBar.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { HamburgerMenuIcon, Cross1Icon } from '@radix-ui/react-icons';
 
 import { Logo } from '../index';
 import profileImg from '../../assets/images/profile.png';
@@ -23,31 +24,17 @@ const NavBar = () => {
           ></img>
         )}
         <div onClick={() => setOpen((prev) => !prev)}>
-          {!open ? (
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              height='1em'
-              viewBox='0 0 448 512'
-              className='fill-[#3D5278] w-[1.6rem] h-[1.6rem]'
-            >
-              <path d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z' />
-            </svg>
+          {open ? (
+            <Cross1Icon className='fill-[#3D5278] w-[1.6rem] h-[1.6rem]' />
           ) : (
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              height='1em'
-              viewBox='0 0 384 512'
-              className='fill-[#3D5278] w-[1.6rem] h-[1.6rem]'
-            >
-              <path d='M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z' />
-            </svg>
+            <HamburgerMenuIcon className='fill-[#3D5278] w-[1.6rem] h-[1.6rem]' />
           )}
         </div>
       </div>
 
       {open && (
         <div className='h-full bg-white w-[70%] fixed top-0 left-0 z-50'>
-          <NavMenu isMobile={true} />
+          <NavMenu />
         </div>
       )}
     </header>
