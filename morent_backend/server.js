89ca5c76ed34cf7be20be61e1d9576cde29e6e0d
wirayuda <wirayuda233@gmail.com>
2023-08-16 00:00:33 +0800

    remove stripe

--- morent_backend/server.js
+++ morent_backend/server.js
@@ -3,7 +3,6 @@ dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
-import stripe from 'stripe'
 
 import connectToDatabase from './src/configs/db.js';
 
@@ -14,38 +13,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const stripeInit = stripe(process.env.STRIPE_PRIVATE_KEY)
-
 // SCHEMA
 
 // Routes
 app.get('/message', (req, res) => {
   res.json({ message: 'Hello from the server!' });
-});
-
-// stripe
-app.post('/create-checkout-session', async (req, res) => {
-  const { id, name, price } = req.body
-  const session = await stripeInit.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name,
-          },
-          unit_amount: price,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${process.env.CLIENT_URL}/success`,
-    cancel_url: `${process.env.CLIENT_URL}/cancel`,
-
-  });
-
-  res.send({ url: session.url }).end()
 });
 
 // connect db
