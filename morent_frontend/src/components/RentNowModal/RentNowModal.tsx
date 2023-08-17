14a702d52d4972373c6560522240a7d3b7d3ab2c
Hannah-Liao <hanna.liao@outlook.com>
2023-08-17 20:36:32 +0100

    add time picker to modal

--- morent_frontend/src/components/RentNowModal/RentNowModal.tsx
+++ morent_frontend/src/components/RentNowModal/RentNowModal.tsx
@@ -3,6 +3,7 @@ import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { format } from 'date-fns';
 import { CalendarIcon } from '@radix-ui/react-icons';
+import TimePicker from 'react-time-picker';
 
 import { Button } from '../ui/button';
 import {
@@ -33,39 +34,42 @@ import {
 import { Calendar } from '../ui/calendar';
 import { cn } from '../../lib/utils';
 import { pickupLocation } from '../../constant/index';
+import getCurrentTime from '../../utils/getCurrentTime';
 
 const formSchema = z.object({
   location: z.string({
     required_error: 'Location is required',
   }),
-  startDate: z
+  pickUpDate: z
     .date()
     .min(new Date(), { message: 'Please select a future date' }),
-  endDate: z.date({
-    required_error: 'the date is required',
-  }),
   dropOffDate: z
     .date()
     .min(new Date(), { message: 'Please select a future date' }),
-  dropOfDate: z.date({
-    required_error: 'the date is required',
-  }),
+  pickUpTime: z.string(),
+  dropOffTime: z.string(),
 });
 
 export default function RentNowMadal() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      pickUpDate: new Date(),
+      pickUpTime: getCurrentTime(),
+      dropOffDate: new Date(),
+      dropOffTime: getCurrentTime(),
+    },
   });
 
   const onSubmit = (data: z.infer<typeof formSchema>) => console.log(data);
 
   return (
     <Dialog>
-      {/* <DialogTrigger asChild>
+      <DialogTrigger asChild>
         <Button variant='outline'>Edit Profile</Button>
-      </DialogTrigger> */}
+      </DialogTrigger>
 
-      <DialogContent className='max-w-[500px] shrink-0 rounded-[10px] p-[50px] bg-white dark:bg-gray-850 '>
+      <DialogContent className='max-w-[500px] shrink-0 rounded-[10px] px-[16px] sm:px-[50px] py-[40px] sm:py-[50px] bg-white dark:bg-gray-850 '>
         <DialogHeader className='flex flex-col gap-2.5 mb-7 sm:mb-10'>
           <DialogTitle className='base-bold text-gray-900 dark:text-white'>
             Add Pickup & Drop-Off Info
@@ -90,7 +94,7 @@ export default function RentNowMadal() {
                       onValueChange={field.onChange}
                       defaultValue={field.value}
                     >
-                      <SelectTrigger className='w-full h-[46px] sm:h-[56px] bg-white-200 dark:bg-gray-800 body-regular text-gray-400 dark:text-white-200'>
+                      <SelectTrigger className='border-none h-[46px] sm:h-[56px] bg-white-200 dark:bg-gray-800 body-regular text-gray-400 dark:text-white-200'>
                         <SelectValue placeholder='Location Address' />
                       </SelectTrigger>
                       <SelectContent className='bg-white-200 dark:bg-gray-800 capitalize'>
@@ -109,10 +113,10 @@ export default function RentNowMadal() {
               )}
             />
 
-            <div className='flex flex-col sm:flex-row gap-6 sm:gap-2.5 '>
+            <div className='grid grid-cols-2 gap-2.5'>
               <FormField
                 control={form.control}
-                name='startDate'
+                name='pickUpDate'
                 render={({ field }) => (
                   <FormItem className='flex flex-col'>
                     <FormLabel className='body-semibold sm:p-semibold mb-2.5 sm:mb-4 text-gray-900 dark:text-white flex'>
@@ -124,7 +128,7 @@ export default function RentNowMadal() {
                         <Button
                           variant={'outline'}
                           className={cn(
-                            'w-full sm:w-[195px] h-[56px] justify-start text-left bg-white-200 dark:bg-gray-800 body-regular text-gray-400 dark:text-white-200',
+                            'border-none h-[56px] justify-start text-left bg-white-200 dark:bg-gray-800 body-regular text-gray-400 dark:text-white-200',
                             !field.value && 'text-muted-foreground',
                           )}
                         >
@@ -148,48 +152,30 @@ export default function RentNowMadal() {
                   </FormItem>
                 )}
               />
-              {/* <DateFormField form={...form} /> */}
+
               <FormField
                 control={form.control}
-                name='endDate'
+                name='pickUpTime'
                 render={({ field }) => (
                   <FormItem className='flex flex-col'>
                     <FormLabel className='body-semibold sm:p-semibold mb-2.5 sm:mb-4 text-gray-900 dark:text-white flex'>
                       <CalendarIcon className='mr-2 h-4 w-4 text-blue-500 ' />
                       Availability To
                     </FormLabel>
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <Button
-                          variant={'outline'}
-                          className={cn(
-                            'w-full sm:w-[195px] h-[56px] justify-start text-left bg-white-200 dark:bg-gray-800 body-regular text-gray-400 dark:text-white-200',
-                            !field.value && 'text-muted-foreground',
-                          )}
-                        >
-                          {field.value ? (
-                            format(field.value, 'LLL dd, y')
-                          ) : (
-                            <span>Select your date</span>
-                          )}
-                        </Button>
-                      </PopoverTrigger>
-                      <PopoverContent className='w-auto p-0'>
-                        <Calendar
-                          mode='single'
-                          selected={field.value}
-                          onSelect={field.onChange}
-                          initialFocus
-                        />
-                      </PopoverContent>
-                    </Popover>
+                    <TimePicker
+                      disableClock
+                      clearIcon
+                      onChange={field.onChange}
+                      value={field.value}
+                      id='modalpickUpTime'
+                    />
                     <FormMessage />
                   </FormItem>
                 )}
               />
             </div>
 
-            <div className='flex flex-col sm:flex-row gap-6 sm:gap-2.5 '>
+            <div className='grid grid-cols-2 gap-2.5'>
               <FormField
                 control={form.control}
                 name='dropOffDate'
@@ -204,7 +190,7 @@ export default function RentNowMadal() {
                         <Button
                           variant={'outline'}
                           className={cn(
-                            'w-full sm:w-[195px] h-[56px] justify-start text-left bg-white-200 dark:bg-gray-800 body-regular text-gray-400 dark:text-white-200',
+                            'border-none h-[56px] justify-start text-left bg-white-200 dark:bg-gray-800 body-regular text-gray-400 dark:text-white-200',
                             !field.value && 'text-muted-foreground',
                           )}
                         >
@@ -231,38 +217,20 @@ export default function RentNowMadal() {
 
               <FormField
                 control={form.control}
-                name='endDate'
+                name='dropOffTime'
                 render={({ field }) => (
                   <FormItem className='flex flex-col'>
                     <FormLabel className='body-semibold sm:p-semibold mb-2.5 sm:mb-4 text-gray-900 dark:text-white flex'>
                       <CalendarIcon className='mr-2 h-4 w-4 text-blue-500 ' />
                       Availability To
                     </FormLabel>
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <Button
-                          variant={'outline'}
-                          className={cn(
-                            'w-full sm:w-[195px] h-[56px] justify-start text-left bg-white-200 dark:bg-gray-800 body-regular text-gray-400 dark:text-white-200',
-                            !field.value && 'text-muted-foreground',
-                          )}
-                        >
-                          {field.value ? (
-                            format(field.value, 'LLL dd, y')
-                          ) : (
-                            <span>Select your date</span>
-                          )}
-                        </Button>
-                      </PopoverTrigger>
-                      <PopoverContent className='w-auto p-0'>
-                        <Calendar
-                          mode='single'
-                          selected={field.value}
-                          onSelect={field.onChange}
-                          initialFocus
-                        />
-                      </PopoverContent>
-                    </Popover>
+                    <TimePicker
+                      disableClock
+                      clearIcon
+                      id='modaldropOffTime'
+                      onChange={field.onChange}
+                      value={field.value}
+                    />
                     <FormMessage />
                   </FormItem>
                 )}
