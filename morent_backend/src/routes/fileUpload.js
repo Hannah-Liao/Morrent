f77089360f3ea94cee8819444bbe35c38fac301d
wirayuda <wirayuda233@gmail.com>
2023-08-26 12:42:48 +0800

    add authenticate user middleware

--- morent_backend/src/routes/fileUpload.js
+++ morent_backend/src/routes/fileUpload.js
@@ -4,13 +4,21 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import fileUploadMiddleware from '../middleware/fileUploadMiddleware.js';
+import { authenticateUser } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.post('/upload', fileUploadMiddleware, async (req, res) => {
-  const files = req.files.map((file) => `${process.env.BASE_URL}/${file.path}`);
+router.post(
+  '/upload',
+  authenticateUser,
+  fileUploadMiddleware,
+  async (req, res) => {
+    const files = req.files.map(
+      (file) => `${process.env.BASE_URL}/${file.path}`
+    );
 
-  res.json(files);
-});
+    res.json(files);
+  }
+);
 
 export default router;
