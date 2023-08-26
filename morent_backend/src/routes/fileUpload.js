331f809192a26da8b9eade2a7687c47298f7030e
wirayuda <wirayuda233@gmail.com>
2023-08-26 12:33:54 +0800

    refactor file upload, remove cloudinary and use multer to store locally

--- morent_backend/src/routes/fileUpload.js
+++ morent_backend/src/routes/fileUpload.js
@@ -1,28 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
-import multer from 'multer';
+dotenv.config();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
+import fileUploadMiddleware from '../middleware/fileUploadMiddleware.js';
+
 const router = express.Router();
 
-router.post('/upload', upload.array('photos', 4), function (req, res, next) {
-  // req.files is array of `profile-files` files
-  // req.body will contain the text fields, if there were any
-  let response = '<a href="/">Home</a><br>';
-  response += 'Files uploaded successfully.<br>';
-  for (var i = 0; i < req.files.length; i++) {
-    response += `<img src="${req.files[i].path}" /><br>`;
-  }
+router.post('/upload', fileUploadMiddleware, async (req, res) => {
+  const files = req.files.map((file) => `${process.env.BASE_URL}/${file.path}`);
 
-  return res.send(response);
+  res.json(files);
 });
 
 export default router;
