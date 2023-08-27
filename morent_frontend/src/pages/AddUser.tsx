bcee0427da9f38e552dbd07f7019523d16806747
surajarya130 <surajarya130@gmail.com>
2023-08-27 15:34:21 +0530

    resolved conflicts

--- morent_frontend/src/pages/AddUser.tsx
+++ morent_frontend/src/pages/AddUser.tsx
@@ -12,7 +12,10 @@ const userFormSchema = z.object({
     .min(2, { message: 'Must have 2 chars' })
     .max(50, { message: 'Max 50 chars allowed' }),
 
-  email: z.coerce.string().email().min(5, { message: 'Write valid mail' }),
+  email: z.coerce
+    .string()
+    .email({ message: 'Write valid email' })
+    .min(5, { message: 'Too small' }),
 
   phoneNumber: z.coerce.number().min(5).max(12),
   profileBanner: z.string(),
