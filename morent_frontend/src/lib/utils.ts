4392ab4a362e1ab0b4480f09362f0a332d5af4ae
surajarya130 <surajarya130@gmail.com>
2023-08-29 11:40:22 +0530

    some minor fix

--- morent_frontend/src/lib/utils.ts
+++ morent_frontend/src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export interface ApiErrorResponse {
+  status: number;
+  data: { message: string; errors: { [k: string]: string[] } };
+}
+
+export function isApiResponse(error: unknown): error is ApiErrorResponse {
+  return (
+    typeof error === 'object' &&
+    error != null &&
+    'status' in error &&
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    typeof (error as any).status === 'number'
+  );
+}
