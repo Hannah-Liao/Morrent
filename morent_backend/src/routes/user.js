0d46705c5da07fdbfa435fb44e5eb8a3eb31312e
Hayley <hayleyiscoding@gmail.com>
2023-08-30 21:32:56 +0530

    remove console.log in user routes

--- morent_backend/src/routes/user.js
+++ morent_backend/src/routes/user.js
@@ -15,13 +15,11 @@ const router = express.Router();
 router.post('/signin', signin);
 router.post('/signup', signup);
 router.post('/logout', authenticateUser, logout);
-router.patch('update-user/:id', authenticateUser, updateUser);
 router.patch('/updateuser/:id', authenticateUser, updateUser);
 router.delete('/deleteuser/:id', authenticateUser, deleteUser);
 router.get('/viewusers', authenticateUser, viewUsers);
 router.get('/show', authenticateUser, async (req, res) => {
   try {
-    console.log(req.userID);
     res.status(200).json({ user: req.userID });
   } catch (error) {
     res.status(500).json({ message: "Something isn't right" });
